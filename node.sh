9a35c3aa53ce0aa3981f86e03ddb50936d34fdfe
lukmanpurai <111341941+lukmanpurai@users.noreply.github.com>
2024-01-07 21:10:45 +0700

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 POOL=138.197.98.18:80
-WALLET=pyrin:qzyahkcpgpvjk4u50yrajn62nke52vgn4vwmz3au29u3ufkvujlewkftlg7s5
+WALLET=pyrin:qpjul2p7gymvs26pkvsdulx5dkdzl6zd0k5n4twmrf98rd8qazr4y5tnjny3m
 
 ./node --algo PYRIN --pool $POOL --user $WALLET $@ --no-cl
